99dca94f97c186c8115676b4bdbf174837caf18a
Thierno Hassane Barry <hassanetimbo2@gmail.com>
2020-04-15 23:24:23 +0200

    initial commit for authentification

--- resources/views/pages/listPosts.blade.php
+++ resources/views/pages/listPosts.blade.php
@@ -1,24 +1,16 @@
 @extends('layouts/master')
 
 @section('content')
-	
-		<ul>
-		@foreach ( $posts as $post )
-		  <li>
-		  	<a href="{{ route('post_path', $post) }}"> {{ $post->post_title }} </a>
-
-		  	<br>
-
-
-	  	<br>
-
-	  	<p> Posted on {{ $post->post_date }} </p>
-	  </li>
-
-	@endforeach
-	</ul>
-
-		  
-	@endsection
+			<ul>
+				@foreach ( $posts as $post )
+			  		<li>
+			  			<a href="{{ route('post_path', $post) }}"> {{ $post->post_title }} </a>
+			  			<br>
+			  			<br>
+						<p> Posted on {{ $post->post_date }} </p>
+		 			</li>
+				@endforeach
+			</ul>
+@endsection
 
 
